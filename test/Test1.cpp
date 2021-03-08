2718efe0f75cfd4eae4cb3393c0e1863152523e2
stas <q>
2021-03-08 23:00:10 +0200

    add tests

--- test/Test1.cpp
+++ test/Test1.cpp
@@ -3,10 +3,11 @@
 
 // Google Test test cases are created using a C++ preprocessor macro
 // Here, a "test suite" name and a specific "test name" are provided.
-TEST(module_name, test_name) {
+TEST(foo, test_name1) {
     std::cout << "Hello world!" << std::endl;
     // Google Test will also provide macros for assertions.
     ASSERT_EQ(1 + 1, 2);
+
 }
 
 // Google Test can be run manually from the main() function
